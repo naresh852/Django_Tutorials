c483924f9fb5ee74ea8b8f744fc755ef2055dd7c
naresh852 <macharamnareshreddy321@gmail.com>
2021-03-28 18:52:33 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+''' pip install django
+python -m django --version #to check django version
+django-admin help subcommand
+django-admin startproject django_project  #to start project
+tree
+py manage.py runserver  ##to start  website
+#to see all files in it
+'''#http://127.0.0.1:8000/admin ##to see login page'''
+'''
+py manage.py startapp blog  #to to create new app with a name blog
+/blog   ## to see blog page
+'''
+'''
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/'''
