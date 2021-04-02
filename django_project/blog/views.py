f7b822bdfdfaea09fc2700c066f00be82df7adac
naresh852 <macharamnareshreddy321@gmail.com>
2021-04-02 12:28:12 +0530

    added posts sql DB used Post
    added posts sql DB used Post

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
+from . models import Post
 # from django.http import HttpResponse
 # Create your views here.
-posts = [
-   {
-       'author':'Coreyms',
-       'title':'Blog posts',
-       'content':'First post content',
-       'date_posted':'August 27, 2018'
-   },
-    {
-       'author':'naresh',
-       'title':'Blog posts 2',
-       'content':'second post content',
-       'date_posted':'August 28, 2018'
-   }
+# posts = [
+#    {
+#        'author':'Coreyms',
+#        'title':'Blog posts',
+#        'content':'First post content',
+#        'date_posted':'August 27, 2018'
+#    },
+#     {
+#        'author':'naresh',
+#        'title':'Blog posts 2',
+#        'content':'second post content',
+#        'date_posted':'August 28, 2018'
+#    }
 
-]
+# ]
 ##this will handle the traffic from homepage
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 # def about(request):
--- main.py
+++ main.py
@@ -26,9 +26,119 @@ then add context in views,then in home html for loop contents
 
 ############tut 3 ADMIN PAGES ################
 py manage.py createsuperuser  #THROWS ERROR no such table: auth_user BECAUSE WE HAVENT CREATED DB YET
-py manage.py makemigrations  #we havent added db yet
+py manage.py makemigrations  #we havent added db yet detect changes
 py manage.py migrate  ##now we can use admin page
 py manage.py createsuperuser  #add user passwd email    
+
+######### DB AND MIGRATIONS ############
+ORM OBJECT REATIONAL MATTER
+SQLLITE FOR DEVELOPMENT ,POSTGRESQL FOR PRODUCITON
+WE HAVE MADE MODEL AND NOW WE NEED TO MIGRATE IT TO DB
+py manage.py makemigrations  #SO WE USE THIS COMMAND
+py manage.py sqlmigrate blog 0001 #to ssee how model code exctes in sql code
+py manage.py migrate   #to migrate to DB
+
+from blog.models import Post
+ from django.contrib.auth.models import User
+ User.objects.all()
+ User.objects.first()
+Out[4]: <User: naresh>
+
+In [5]: User.objects.filter(username='naresh')
+Out[5]: <QuerySet [<User: naresh>]>
+
+In [6]: User.objects.filter(username='naresh').first()
+Out[6]: <User: naresh>
+
+In [7]: user=User.objects.filter(username='naresh').first()  
+ user
+Out[8]: <User: naresh>
+
+In [9]: user.id
+Out[9]: 1
+
+In [10]: user.pk
+Out[10]: 1
+
+In [11]: user= User.objects.get(id=1)
+
+In [12]: user
+Out[12]: <User: naresh>
+
+In [13]: Post.objects.all()
+Out[13]: <QuerySet []>
+
+In [14]: post_1 = Post(title='Blog_1',content='First post content' 
+    ...: ,author=user)
+
+In [15]: Post.objects.all()
+Out[15]: <QuerySet []>
+
+
+In [19]: post_1.save()
+
+In [20]: Post.objects.all()
+Out[20]: <QuerySet [<Post: Post object (1)>]>
+v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)]
+Type 'copyright', 'credits' or 'license' for more information      
+IPython 7.21.0 -- An enhanced Interactive Python. Type '?' for help.
+
+In [1]: from blog.models import Post
+
+In [2]: from django.contrib.auth.models import User
+
+In [3]: Post.objects.all()
+Out[3]: <QuerySet [<Post: Blog_1>]>
+
+In [4]: user = User.objects.filter(username='naresh').first()      
+
+In [5]: user
+Out[5]: <User: naresh>
+
+
+In [7]: post_2 = Post(title='Blog 2',content='Second Post Content' 
+   ...: ,author_id=user.id)
+
+In [8]: post_2
+Out[8]: <Post: Blog 2>
+
+In [9]: post_2.save()
+
+In [10]: Post.objects.all()
+Out[10]: <QuerySet [<Post: Blog_1>, <Post: Blog 2>]>
+
+In [11]: post = Post.objects.first()
+
+In [12]: post.content
+Out[12]: 'First post content'
+
+In [13]: post.date_posted
+Out[13]: datetime.datetime(2021, 4, 2, 5, 26, 21, 794766, tzinfo=<UTC>)
+
+In [14]: post.author
+Out[14]: <User: naresh>
+
+In [15]: post.author.email
+Out[15]: 'djangonaresh123@gmail.com'
+
+In [16]: user
+Out[16]: <User: naresh>
+
+In [17]: user.post_set
+Out[17]: <django.db.models.fields.related_descriptors.create_reverse_many_to_one_manager.<locals>.RelatedManager at 0x163e0f37c40>    
+
+In [18]: user.post_set.all
+Out[18]: <bound method BaseManager.all of <django.db.models.fields.related_descriptors.create_reverse_many_to_one_manager.<locals>.RelatedManager object at 0x00000163E0EDC190>>
+
+In [19]: user.post_set.all()
+Out[19]: <QuerySet [<Post: Blog_1>, <Post: Blog 2>]>
+
+In [20]: user.post_set.create(title='Blog 3',content='Third post c 
+    ...: ontent!')
+Out[20]: <Post: Blog 3>
+
+In [21]: Post.objects.all()
+Out[21]: <QuerySet [<Post: Blog_1>, <Post: Blog 2>, <Post: Blog 3>]>
 
 go to django model reference,charfield to see models
 py manage.py makemigrations # to make migrations
